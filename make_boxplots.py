2f8b07485332e44bcc83020ca7af8d004a97c2cf
Andrew Loeppky <Andrew.Loeppky@Gmail.com>
2020-11-05 12:28:27 +0100

    add cumulative INP function

--- make_boxplots.py
+++ make_boxplots.py
@@ -13,8 +13,8 @@ Counter (FINC) and generating plots.
 
 TODO:
 replicate Killian's heatmap of freeze order, frozen fraction 
-curve, create a processing function to produce diff and cumulative
-freezing temperatures
+curve, create a processing function to produce diff and 
+cumulative freezing temperature curves
 ===============================================================
 """
 # %%
@@ -34,10 +34,10 @@ def get_mat(the_file):
     """
     the_path = "C:/Users/Owner/UBC_F2020/FINC/outputs/"
     load_this = the_path + the_file + "/workspace.mat"
-
     mat = sio.loadmat(load_this)
     frzTall = mat["FrzTall"]
-    frzTall = np.sort(frzTall, axis=0)
+    # frzTall = np.sort(frzTall, axis=0)
+
     return frzTall
 
 
@@ -76,20 +76,54 @@ def make_boxplot(*data):
 
 
 # %%
+def make_ff_curve(data):
+    """
+    creates a frozen fraction (FF) curve as a function of
+    temperature
+    """
+    data = np.sort(data, axis=0)
+    ind = np.linspace(len(data), 1, len(data)) / len(data)
+    plt.plot(data, ind)
+    plt.xlabel("Temp ($^oC$")
+    plt.ylabel("FF")
+
+
+# %%
+def make_big_K(data, norm=1, Vwell=10):
+    """
+    creates a cumulative concentration curve from Vali eq.
+
+    default normalization constant (eg surface area) = 1
+    default well volume = 10 (microlitre)
+    """
+    # make frozen fraction curve
+    data = np.sort(data, axis=0)
+    ind = np.linspace(len(data), 1, len(data)) / len(data)
+
+    K = -np.log(1 - ind) / (norm * Vwell * 10 ** -6) # Vali eq. 
+
+    # plot
+    plt.plot(data, K)
+    plt.yscale('log')
+    plt.xlabel('Temp ($^oC$)')
+    plt.ylabel('K(T) ($L^{-1}$)')
+# %%
 def main():
     # name of expt runs
     SA1 = "finc_20201007_SA1"
     SA2 = "finc_20201007_SA2"
     SA3 = "finc_20201007_SA3"
+    lig = "finc_20201028_lignin2"
 
     # get the data from matlab
     SA1 = get_mat(SA1)
     SA2 = get_mat(SA2)
     SA3 = get_mat(SA3)
+    lig = get_mat(lig)
 
     # make some plots
-    make_hist(SA1)
-    make_boxplot(SA1, SA2, SA3)
+    make_hist(lig)
+    make_boxplot(SA1, SA2, SA3, lig)
 
 
 if __name__ == "__main__":
