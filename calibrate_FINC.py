f7911283b03d8f804eac17357941bb36dd48ba7f
Andrew Loeppky <Andrew.Loeppky@Gmail.com>
2020-12-14 14:54:30 +0100

    checkpoint, tc reader still broken

--- calibrate_FINC.py
+++ calibrate_FINC.py
@@ -7,7 +7,8 @@
 # Author: Andrew Loeppky
 # NBD Group, ETH Zurich, Dec 11/2020
 #
-# Make sure to change default temp controller format .XLS to .csv
+# Make sure to change default temp controller format .XLS to .txt
+# My favorite format for most data is dictionaries full of np arrays
 #
 # ============================================================
 
@@ -15,7 +16,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from pathlib import Path
-import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.stats import linregress
 import datetime
@@ -26,58 +26,81 @@ def count_lines(txt_file):
     counts the number of lines in a given txt file
     input the folder path, returns int
     """
-    count = -1 # include 0 as index for creating temp and time arrays
+    count = -1  # include 0 as index for creating temp and time arrays
     for line in open(txt_file):
         count += 1
-    return count 
+    return count
 
 
 def get_finc_ramp(file_path):
-    '''
+    """
     Input: ramp.txt from FINCrunner.m
     specify absolute file path
 
     Out: dictionary containing datetime and lauda temp
-    '''
-    finc_time = np.empty(count_lines(file_path),dtype='datetime64[us]')
+    """
+    finc_time = np.empty(count_lines(file_path), dtype="datetime64[us]")
     finc_temp = np.empty(count_lines(file_path))
     i = 0
 
     for line in open(file_path):
         try:
-            the_str = line.split(',')
+            the_str = line.split(",")
             # get datetime and reformat to np.datetime
             # https://stackabuse.com/converting-strings-to-datetime-in-python/
-            dt = datetime.datetime.strptime(the_str[0], '%d-%b-%Y %H:%M:%S')
+            dt = datetime.datetime.strptime(the_str[0], "%d-%b-%Y %H:%M:%S")
             finc_time[i] = np.datetime64(dt)
-            #npdt = np.datetime64(dt)
-            #finc_time[i] = npdt
-            
-            #get the temp
+
+            # get the temp
             finc_temp[i] = the_str[1]
             i += 1
         except ValueError:
             # omit metadata lines
             pass
-        
-    
-    plt.plot(finc_time,finc_temp)
-    
+
+    # create and return the dictionary full of np arrays
+    out_dict = {}
+    out_dict["finc_time"] = finc_time
+    out_dict["finc_temp"] = finc_temp
+    return out_dict
 
 
 def get_tc_ramp(file_path):
-    '''
-    Input: Save temp controller file as .csv and change
+    """
+    Input: Save temp controller file as .txt (tab delimited) and change
     nothing else. Specify absolute path to where file is saved.
-    
+
     Output: 12 thermocouple time series and a datetime list
     as a dictionary.
-    '''
-    tc_panda = pd.read_csv(file_path)
-    tc_dict = {}
-    tc_dict['datetime'] = tc_panda['Date'] + ' ' + tc_panda['Time']
-    print(tc_dict)
+    """
 
+    tc_time = np.empty(count_lines(file_path), dtype="datetime64[us]")
+    tc1 = np.empty(count_lines(file_path))
+    tc2 = np.empty_like(tc1)
+    i = 0
+    omitted_lines = 0
+
+    for line in open(file_path):
+        # get the time
+        try:
+            the_str = line.split(",")
+            raw_date = the_str[1] + " " + the_str[2]
+            dt = datetime.datetime.strptime(raw_date, "%Y-%m-%d %H:%M:%S")
+            tc_time[i] = np.datetime64(dt)
+            print(tc_time[i])
+            # get 12 thermocouple temperatures
+            tc1[i] = the_str[3]
+            tc2[i] = the_str[4]
+            i += 1
+        
+        except ValueError:
+            # omit lines containing metadata
+            omitted_lines += 1
+
+    print(f'Omitted {omitted_lines} lines')
+
+    
+        
 
 
 # finc data
@@ -85,10 +108,9 @@ finc_ramp_path = "C:/Users/Owner/UBC_F2020/FINC/outputs/20201209_calibrate/ramp.
 # thermocouple data
 tc_ramp_path = "C:/Users/Owner/UBC_F2020/FINC/outputs/20201209_calibrate/TMB01001.csv"
 
-#get_tc_ramp(tc_ramp_path)
+get_tc_ramp(tc_ramp_path)
 
-get_finc_ramp(finc_ramp_path)
-
+# a = get_finc_ramp(finc_ramp_path)
 
 
 # %%
